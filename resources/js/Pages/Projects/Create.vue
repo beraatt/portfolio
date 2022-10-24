e54c27f6e49cfd593900df462afa71a95044f834
beraatt <113882776+beraatt@users.noreply.github.com>
2022-10-24 13:30:14 +0300

    x

---
+++ resources/js/Pages/Projects/Create.vue
@@ -0,0 +1,25 @@
+<template>
+    <Head title="New Project" />
+    <AuthenticatedLayout>
+           <template #header>
+               <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                New Project
+               </h2>
+           </template>
+
+           <div class="py-12">
+               <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end m-2 p-2">
+                    <Link :href="route('projects.index')"
+                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">Back To Projects</Link>
+                </div>
+                Form
+               </div>
+           </div>
+       </AuthenticatedLayout>
+
+   </template>
+   <script setup>
+   import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+   import { Head, Link } from '@inertiajs/inertia-vue3';
+   </script>
